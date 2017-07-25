2fb3b084c46a3cf6b375bcff9c00e0f14c8cc5c0
PharaohKJ <kato+github@phalanxware.com>
2017-07-25 20:55:36 +0900

    1st commit

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,5 +16,20 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var testFilePath = System.Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\test.xlsx";
+            System.IO.File.Copy(
+                @"C:\Users\真透\Downloads\領収書テンプレート.xlsx",
+                testFilePath,
+                true
+                );
+            var a = new ClosedXML.Excel.XLWorkbook(testFilePath);
+            var ws = a.Worksheet(1);
+            MessageBox.Show(ws.Cell("A4").Value.ToString());
+            ws.Cell("A4").Value = "ファランクスウェア";
+            a.Save();
+        }
     }
 }
